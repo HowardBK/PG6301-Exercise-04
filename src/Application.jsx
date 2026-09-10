997f909bf4d7d59f4876833f7283203867b54ba8
HowardBK <dazmhbk@gmail.com>
2026-09-10 15:46:15 +0200

    add task to list

--- src/Application.jsx
+++ src/Application.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
+
 export default function Application() {
+  const [tasks, setTasks] = useState([
+    { description: "Task1" },
+    { description: "Task2" },
+    { description: "Task3" },
+  ]);
+
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTasks((old) => [...old, { description }]);
+  }
+
   return (
     <>
       <h1>My tasks</h1>
       <ul>
-        <li>First Task</li>
+        {tasks.map((t) => (
+          <li>
+            <input type={"checkbox"} />
+            {t.description}
+          </li>
+        ))}
+      </ul>
+      <h2>New Task</h2>
+      <ul>
+        <form onSubmit={handleSubmit}>
+          <div>
+            Task Name:
+            <input
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div>
+            <button>Save {description}</button>
+          </div>
+        </form>
       </ul>
     </>
   );
